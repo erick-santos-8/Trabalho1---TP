da6d5cf759102dffccf09f5a31fd9c8a512e5724
Bastres <109953748+Bastres@users.noreply.github.com>
2023-10-19 22:44:26 -0300

    Update testes.h

--- Trabalho TP/KanbanLib/testes.h
+++ Trabalho TP/KanbanLib/testes.h
@@ -23,4 +23,18 @@ public:
     int run();                              // Método para executar teste.
 };
 
+class TUProjeto {
+private:
+    const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
+    Projeto *projeto;                       // Referência para unidade em teste.
+    int estado;                             // Estado do teste.
+    void setUp();                           // Método para criar unidade em teste.
+    void tearDown();                        // Método para destruir unidade em teste.
+    void testarCenarioSucesso();            // Cenário de teste.
+public:
+    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
+    int run();                              // Método para executar teste.
+};
+
 #endif // TESTES_H_INCLUDED
