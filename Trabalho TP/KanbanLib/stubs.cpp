854b7137eb8ff77ad5afdd089208663cbb89d43c
PedroAmorimG <pedroamorim696@gmail.com>
2023-12-03 19:26:32 -0300

    mudancas nas stubs

--- Trabalho TP/KanbanLib/stubs.cpp
+++ Trabalho TP/KanbanLib/stubs.cpp
@@ -2,74 +2,77 @@
 
 //--------------------------------------------------------------------------------------------
 // Valores usados como gatilhos de erros.
+const string StubISAutenticacao::EMAIL_INVALIDO = "teste@testemail";
+const string StubISAutenticacao::SENHA_INVALIDA = "Ab1!a";
 
-const string StubISAutenticacao::INVALIDO = "12345";
-const string StubISConta::INVALIDO = "12345";
-const string StubISProjetos::INVALIDO = "12345";
+const string StubISConta::EMAIL_INVALIDO = "teste@testemail";
+const string StubISConta::SENHA_INVALIDA = "Ab1!a";
+
+const string StubISProjetos::CODIGO_INVALIDO = "AB12";
 
 //--------------------------------------------------------------------------------------------
 // Implementa��es dos m�todos de classes stub.
 
-bool StubISAutenticacao::autenticar(Email email, Senha senha){
-    if(email.getEmail().compare(INVALIDO) == 0)
-        return false;
-    if(senha.getSenha().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISAutenticacao::autenticar(Email email, Senha senha) {
+  if (email.getEmail() == EMAIL_INVALIDO)
+    return false;
+  if (senha.getSenha() == SENHA_INVALIDA)
+    return false;
+  return true;
 }
 
-bool StubISConta::criarConta(Conta conta){
-    if(conta.getEmail().getEmail().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISConta::criarConta(Conta conta) {
+  if (conta.getEmail().getEmail() == EMAIL_INVALIDO)
+    return false;
+  return true;
 }
 
-bool StubISConta::eliminar(Email email){
-    if(email.getEmail().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISConta::eliminar(Email email) {
+  if (email.getEmail() == EMAIL_INVALIDO)
+    return false;
+  return true;
 }
 
-bool StubISConta::editar(Conta conta){
-    if(conta.getEmail().getEmail().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISConta::editar(Conta conta) {
+  if (conta.getEmail().getEmail() == EMAIL_INVALIDO)
+    return false;
+  return true;
 }
 
-bool StubISConta::visualizar(Conta *conta){
-    if(conta->getEmail().getEmail().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISConta::visualizar(Conta *conta) {
+  if (conta->getEmail().getEmail() == EMAIL_INVALIDO)
+    return false;
+  return true;
 }
 
-bool StubISProjetos::criarQuadro(Quadro quadro){
-    if(quadro.getCodigo().getCodigo().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISProjetos::criarQuadro(Quadro quadro) {
+  if (quadro.getCodigo().getCodigo() == CODIGO_INVALIDO)
+    return false;
+  return true;
 }
 
-bool StubISProjetos::eliminarQuadro(Codigo codigo){
-    if(codigo.getCodigo().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISProjetos::eliminarQuadro(Codigo codigo) {
+  if (codigo.getCodigo() == CODIGO_INVALIDO)
+    return false;
+  return true;
 }
-bool StubISProjetos::criarCartao(Cartao cartao){
-    if(cartao.getCodigo().getCodigo().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISProjetos::criarCartao(Cartao cartao) {
+  if (cartao.getCodigo().getCodigo() == CODIGO_INVALIDO)
+    return false;
+  return true;
 }
-bool StubISProjetos::visualizarCartao(Codigo *codigo){
-    if(codigo->getCodigo().compare(INVALIDO)==0)
-        return false;
-    return true;
+bool StubISProjetos::visualizarCartao(Codigo *codigo) {
+  if (codigo->getCodigo() == CODIGO_INVALIDO)
+    return false;
+  return true;
 }
-bool StubISProjetos::moverCartao(Codigo codigo){
-    if(codigo.getCodigo().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISProjetos::moverCartao(Codigo codigo) {
+  if (codigo.getCodigo() == CODIGO_INVALIDO)
+    return false;
+  return true;
 }
-bool StubISProjetos::eliminarCartao(Codigo codigo){
-    if(codigo.getCodigo().compare(INVALIDO) == 0)
-        return false;
-    return true;
+bool StubISProjetos::eliminarCartao(Codigo codigo) {
+  if (codigo.getCodigo() == CODIGO_INVALIDO)
+    return false;
+  return true;
 }
--- Trabalho TP/KanbanLib/stubs.h
+++ Trabalho TP/KanbanLib/stubs.h
@@ -1,51 +1,55 @@
 #ifndef STUBS_H_INCLUDED
 #define STUBS_H_INCLUDED
 
-#include <string>
 #include "dominios.h"
 #include "entidades.h"
 #include "interfaces.h"
+#include <string>
 
 using namespace std;
 
 //--------------------------------------------------------------------------------------------
 // Declara��es de classes stub.
 
-class StubISAutenticacao:public ISAutenticacao {
-    private:
-        static const string INVALIDO;
-    public:
-        bool autenticar(Email, Senha);
+class StubISAutenticacao : public ISAutenticacao {
+private:
+  static const string EMAIL_INVALIDO;
+  static const string SENHA_INVALIDA;
+
+public:
+  bool autenticar(Email, Senha);
 };
 
 //--------------------------------------------------------------------------------------------
 
-class StubISConta:public ISConta {
-    private:
-        static const string INVALIDO;
-    public:
-        bool criarConta(Conta);
-        bool eliminar(Email);
-        bool editar(Conta);
-        bool visualizar(Conta *);
+class StubISConta : public ISConta {
+private:
+  static const string EMAIL_INVALIDO;
+  static const string SENHA_INVALIDA;
+
+public:
+  bool criarConta(Conta);
+  bool eliminar(Email);
+  bool editar(Conta);
+  bool visualizar(Conta *);
 };
 
 //--------------------------------------------------------------------------------------------
 
-class StubISProjetos:public ISProjetos {
-    private:
-        static const string INVALIDO;
-    public:
-        // metodos para quadro
-        bool criarQuadro(Quadro);
-        bool eliminarQuadro(Codigo);
+class StubISProjetos : public ISProjetos {
+private:
+  static const string CODIGO_INVALIDO;
 
-        // metodos para cartao
-        bool criarCartao(Cartao);
-        bool visualizarCartao(Codigo *);
-        bool moverCartao(Codigo);
-        bool eliminarCartao(Codigo);
+public:
+  // metodos para quadro
+  bool criarQuadro(Quadro);
+  bool eliminarQuadro(Codigo);
+
+  // metodos para cartao
+  bool criarCartao(Cartao);
+  bool visualizarCartao(Codigo *);
+  bool moverCartao(Codigo);
+  bool eliminarCartao(Codigo);
 };
 
 #endif // STUBS_H_INCLUDED
-
